a6475ef33000f6348a6a4a72ec1955146fb9f040
kibukj <kibuzhj@gmail.com>
2021-01-24 09:18:19 -0500

    Poodle's Palace base resource customizations

--- resources/[gamemodes]/[maps]/redm-map-one/map.lua
+++ resources/[gamemodes]/[maps]/redm-map-one/map.lua
@@ -1,2 +1,15 @@
-spawnpoint 'player_three' { x = -262.849, y = 793.404, z = 118.087 }
-spawnpoint 'player_zero' { x = -262.849, y = 793.404, z = 118.087 }
+--spawnpoint 'player_three' { x = -262.849, y = 793.404, z = 118.087 }
+--spawnpoint 'a_f_m_skpprisononline_01' { x = -262.849, y = 793.404, z = 118.087 } -- Valentine
+--spawnpoint 'a_m_m_skpprisononline_01' { x = -262.849, y = 793.404, z = 118.087 } -- Valentine
+--spawnpoint 'player_zero' { x = 1997.57, y = -4499.807, z = 41.77455 } -- Guarma Beach
+--spawnpoint 'player_zero' { x = 999.9134, y = -6749.735, z = 63.12267 } -- Guarma Fort
+
+-- Spawn as a random prisoner on Sisika
+spawnpoint 'a_m_m_skpprisoner_01' { x = 3350.55, y = -642.22, z = 45.29, heading = 40 } -- Sisika
+spawnpoint 'a_m_m_skpprisonline_01' { x = 3350.55, y = -642.22, z = 45.29, heading = 40 } -- Sisika
+spawnpoint 'a_f_m_skpprisononline_01' { x = 3350.55, y = -642.22, z = 45.29, heading = 40 } -- Sisika
+spawnpoint 'mp_jailbreak_males_01' { x = 3350.55, y = -642.22, z = 45.29, heading = 40 } -- Sisika
+spawnpoint 'mp_u_m_m_outlaw3_prisoner_01' { x = 3350.55, y = -642.22, z = 45.29, heading = 40 } -- Sisika
+spawnpoint 'mp_u_m_m_outlaw3_prisoner_02' { x = 3350.55, y = -642.22, z = 45.29, heading = 40 } -- Sisika
+spawnpoint 're_loneprisoner_males_01' { x = 3350.55, y = -642.22, z = 45.29, heading = 40 } -- Sisika
+spawnpoint 're_prisonwagon_males_01' { x = 3350.55, y = -642.22, z = 45.29, heading = 40 } -- Sisika
--- resources/[gamemodes]/basic-gamemode/basic_client.lua
+++ resources/[gamemodes]/basic-gamemode/basic_client.lua
@@ -1,4 +1,39 @@
+--[[
 AddEventHandler('onClientMapStart', function()
   exports.spawnmanager:setAutoSpawn(true)
   exports.spawnmanager:forceRespawn()
 end)
+]]
+
+AddEventHandler('onClientMapStart', function()
+  	exports.spawnmanager:setAutoSpawn(false)
+	exports.spawnmanager:spawnPlayer()
+end)
+--
+--CreateThread(function()
+--	local SendDeathMessage = true
+--
+--	while true do
+--		Wait(0)
+--
+--		if IsPedDeadOrDying(PlayerPedId()) then
+--			if SendDeathMessage then
+--				TriggerEvent('chat:addMessage', {
+--					color = {255, 255, 128},
+--					args = {'You died! Press F to revive or R to respawn.'}
+--				});
+--				
+--				SendDeathMessage = false
+--			end
+--
+--			if IsControlJustPressed(0, 0xE30CD707) then -- Reload, R
+--				exports.spawnmanager:spawnPlayer(nil, function()
+--					SendDeathMessage = true
+--				end)
+--			elseif IsControlJustPressed(0, 0xD596CFB0) then
+--				ResurrectPed(PlayerPedId())
+--				SendDeathMessage = true
+--			end
+--		end
+--	end
+--end)
